b638fc10d881fca6fd0a977213d3807f96672a93
Patrick McGuire <insidenothing@gmail.com>
2017-09-11 16:33:41 -0400

    Update members.php

--- members.php
+++ members.php
@@ -4,7 +4,7 @@ $r = mysqli_query($database, "select * from USERS order by ID");
 ?>
 <table>
  <?PHP 
- while($users=mysqli_fetch_array($r,MYSQL_ASSOC)){
+ while($users=mysqli_fetch_array($r,MYSQLI_ASSOC)){
   echo "<tr><td>$users[NAME]</td><td>$users[EMAIL]</td><td>$users[PERMISSION_LEVEL]</td></tr>";
  }
  ?>
