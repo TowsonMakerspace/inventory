5dede599456b034758573d64ca2bcf55a4d04ac7
Patrick McGuire <insidenothing@gmail.com>
2017-09-11 14:40:11 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?PHP
-$database = mysqli_connect("localhost","root","","inventory");
+// Connect to MySQL
+$link = mysql_connect('localhost', 'root', '');
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+
+// Make my_db the current database
+$db_selected = mysql_select_db('inventory', $link);
+
+if (!$db_selected) {
+  // If we couldn't, then it either doesn't exist, or we can't see it.
+  $sql = 'CREATE DATABASE inventory';
+
+  if (mysql_query($sql, $link)) {
+      echo "Database inventory created successfully\n";
+  } else {
+      echo 'Error creating database: ' . mysql_error() . "\n";
+  }
+}
+
+
 // this is intended to be a single page site
 $config_file = 'settings.json';
 if (!file_exists($config_file)){
@@ -10,5 +30,6 @@ if (!file_exists($config_file)){
 
 
 
+mysql_close($link);
 
 ?>
