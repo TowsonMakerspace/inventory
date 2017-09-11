fafe3a0007e2f3c2ec050cbcef30d129db25c380
Patrick McGuire <insidenothing@gmail.com>
2017-09-11 14:37:52 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?PHP
+$database = mysqli_connect("localhost","root","","inventory");
 // this is intended to be a single page site
 $config_file = 'settings.json';
 if (!file_exists($config_file)){
