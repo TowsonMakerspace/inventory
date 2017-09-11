f54fee2df81bdaacd3b822d9baa7118308aab431
Patrick McGuire <insidenothing@gmail.com>
2017-09-11 16:30:03 -0400

    style

--- menu.php
+++ menu.php
@@ -1,7 +1,7 @@
-<table border="1" width="100%"><tr>
- <td><a href='/inventory/'>HOME</a></td>
- <td><a href='terminal.php'>TERMINAL</a></td>
- <td>Inventory</td>
- <td>Members</td>
- <td>Log Out</td>
+<table border="1" width="100%" cellpadding="5" cellspacing="0"><tr>
+ <td align="center"><a href='/inventory/'>HOME</a></td>
+ <td align="center"><a href='terminal.php'>TERMINAL</a></td>
+ <td align="center"><a href='inventory.php'>Inventory</a></td>
+ <td align="center"><a href='members.php'>Members</a></td>
+ <td align="center"><a href='logout.php'>Log Out<a/></td>
 </tr></table>
