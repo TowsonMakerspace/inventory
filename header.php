1ed78fe3a6b365c14134fa30a7b26d8c5161afb8
Patrick McGuire <insidenothing@gmail.com>
2017-09-11 16:39:14 -0400

    Update header.php

--- header.php
+++ header.php
@@ -9,58 +9,7 @@ if (!file_exists($config_file)){
  die("setup required");
 }
 include_once('settings.php');
-$database = mysqli_connect('localhost', 'root', $database_password, 'inventory');
-/* check connection */
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
-$query = "SELECT ID FROM USERS";
-$result = mysqli_query($database, $query);
-if(empty($result)) {
-         echo "creating table";
-                $query = "CREATE TABLE USERS (
-                          ID int(11) AUTO_INCREMENT,
-                          EMAIL varchar(255) NOT NULL,
-                          NAME varchar(255) NOT NULL,
-                          PASSWORD varchar(255) NOT NULL,
-                          PERMISSION_LEVEL int,
-                          APPLICATION_COMPLETED int,
-                          APPLICATION_IN_PROGRESS int,
-                          PRIMARY KEY  (ID)
-                          )";
-                $result = mysqli_query($database, $query);
-                mysqli_query($database, "insert into USERS (NAME,EMAIL,PASSWORD,PERMISSION_LEVEL) values ('admin','admin@localhost','password','99') ");
-}
-$query = "SELECT ID FROM HARDWARE";
-$result = mysqli_query($database, $query);
-if(empty($result)) {
-         echo "creating table";
-                $query = "CREATE TABLE HARDWARE (
-                          ID int(11) AUTO_INCREMENT,
-                          EMAIL varchar(255) NOT NULL,
-                          PERMISSION_LEVEL int,
-                          APPLICATION_COMPLETED int,
-                          APPLICATION_IN_PROGRESS int,
-                          PRIMARY KEY  (ID)
-                          )";
-                $result = mysqli_query($database, $query);
-}
-$query = "SELECT ID FROM CHECKOUT";
-$result = mysqli_query($database, $query);
-if(empty($result)) {
-         echo "creating table";
-                $query = "CREATE TABLE CHECKOUT (
-                          ID int(11) AUTO_INCREMENT,
-                          hardware_id int,
-                          users_id int,
-                          date_out datetime,
-                          date_back datetime,
-                          PRIMARY KEY  (ID)
-                          )";
-                $result = mysqli_query($database, $query);
-}
-
+include_once('database.php');
 if (isset($_POST['email']) && isset($_POST['password'])){
  echo "$_POST[email] $_POST[password]";
  setcookie("email", "admin@localhost");
