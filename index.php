9c2a293cf18a8bd9c8df77fcfe66dbf3f98be34a
Patrick McGuire <insidenothing@gmail.com>
2017-09-11 15:21:51 -0400

    the 3 tables

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?PHP
-// Connect to MySQL
+// contents of settings.php
+// <?PHP
+// $database_password="your db password";
 $config_file = 'settings.php';
 if (!file_exists($config_file)){
  die("setup required");
 }
 include('settings.php');
 $database = mysqli_connect('localhost', 'root', $database_password, 'inventory');
-
 /* check connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
@@ -27,7 +28,34 @@ if(empty($result)) {
                           )";
                 $result = mysqli_query($database, $query);
 }
-
+$query = "SELECT ID FROM HARDWARE";
+$result = mysqli_query($database, $query);
+if(empty($result)) {
+         echo "creating table";
+                $query = "CREATE TABLE HARDWARE (
+                          ID int(11) AUTO_INCREMENT,
+                          EMAIL varchar(255) NOT NULL,
+                          PERMISSION_LEVEL int,
+                          APPLICATION_COMPLETED int,
+                          APPLICATION_IN_PROGRESS int,
+                          PRIMARY KEY  (ID)
+                          )";
+                $result = mysqli_query($database, $query);
+}
+$query = "SELECT ID FROM CHECKOUT";
+$result = mysqli_query($database, $query);
+if(empty($result)) {
+         echo "creating table";
+                $query = "CREATE TABLE CHECKOUT (
+                          ID int(11) AUTO_INCREMENT,
+                          hardware_id int,
+                          users_id int,
+                          date_out datetime,
+                          date_back datetime,
+                          PRIMARY KEY  (ID)
+                          )";
+                $result = mysqli_query($database, $query);
+}
 // public list of who has what checked out
 
 // public list of available hardware
