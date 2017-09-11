2631af160ffb9a05c9580691a61581fcead8da6e
Patrick McGuire <insidenothing@gmail.com>
2017-09-11 16:28:35 -0400

    Create members.php

---
+++ members.php
@@ -0,0 +1,16 @@
+<?PHP
+include_once('header.php');
+
+
+$r = mysqli_query($database, "select * from USERS order by ID");
+
+
+?>
+
+<table>
+  <?PHP 
+ while($users=mysqli_fetch_array($r,MYSQL_ASSOC)){
+  echo "<tr><td>$users[NAME]</td><td>$users[EMAIL]</td><td>$users[PERMISSION_LEVEL]</td></tr>";
+ }
+ ?>
+ </table>
