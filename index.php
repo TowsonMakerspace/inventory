99a5872b04a07a256467552267b363d494e72b6d
Patrick McGuire <insidenothing@gmail.com>
2017-09-11 14:50:12 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,31 @@
 <?PHP
 // Connect to MySQL
-$link = mysqli_connect('localhost', 'root', '');
-$q = "CREATE DATABASE IF NOT EXISTS inventory";
+$database = mysqli_connect('localhost', 'root', '');
 
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+
+mysqli_query($database, "CREATE DATABASE IF NOT EXISTS inventory");
+
+$query = "SELECT ID FROM USERS";
+$result = mysqli_query($database, $query);
+
+if(empty($result)) {
+                $query = "CREATE TABLE USERS (
+                          ID int(11) AUTO_INCREMENT,
+                          EMAIL varchar(255) NOT NULL,
+                          PASSWORD varchar(255) NOT NULL,
+                          PERMISSION_LEVEL int,
+                          APPLICATION_COMPLETED int,
+                          APPLICATION_IN_PROGRESS int,
+                          PRIMARY KEY  (ID)
+                          )";
+                $result = mysqli_query($dbConnection, $query);
+}
 
 // this is intended to be a single page site
 $config_file = 'settings.json';
