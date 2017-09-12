d21ebdaa0d70853b1a8855fff335ba60ea46f6c6
Patrick McGuire <insidenothing@gmail.com>
2017-09-12 11:48:46 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?PHP
 include_once('header.php');
 // public list of who has what checked out
-echo "<hr>[checkout list]";
-
-// public list of available hardware
-echo "<hr>[available list]";
-
-
-
-
+$r = mysqli_query($database, "select * from CHECKOUT order by ID");
 ?>
+<table>
+ <?PHP 
+ while($checkout=mysqli_fetch_array($r,MYSQLI_ASSOC)){
+  echo "<tr><td>$checkout[hardware_id]</td><td>$checkout[users_id]</td><td>$checkout[date_out]</td><td>$checkout[date_back]</td></tr>";
+ }
+ ?>
+</table>
