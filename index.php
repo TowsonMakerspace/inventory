7b0ab0c00644d44947103710578ede3203571348
Patrick McGuire <insidenothing@gmail.com>
2017-09-12 11:53:30 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include_once('header.php');
 // public list of who has what checked out
 $r = mysqli_query($database, "select * from CHECKOUT order by ID");
 ?>
-<table>
+<table width="100%" border="1" cellpadding="0" cellspacing="0">
  <?PHP 
  while($checkout=mysqli_fetch_array($r,MYSQLI_ASSOC)){
   echo "<tr><td>$checkout[hardware_id]</td><td>$checkout[users_id]</td><td>$checkout[date_out]</td><td>$checkout[date_back]</td></tr>";
