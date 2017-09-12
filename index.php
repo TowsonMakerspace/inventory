9b00dd049a54da2a20c12b165ca30cecdf46c040
Patrick McGuire <insidenothing@gmail.com>
2017-09-12 11:55:36 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,16 @@ include_once('header.php');
 $r = mysqli_query($database, "select * from CHECKOUT order by ID");
 ?>
 <table width="100%" border="1" cellpadding="0" cellspacing="0">
+ <tr>
+  <td>Hardware</td>
+  <td>Member</td>
+  <td style='background-color:orange;'>Checked Out</td>
+  <td style='background-color:green;'>Checked In</td>
+ </tr>
+  
  <?PHP 
  while($checkout=mysqli_fetch_array($r,MYSQLI_ASSOC)){
-  echo "<tr><td>$checkout[hardware_id]</td><td>$checkout[users_id]</td><td>$checkout[date_out]</td><td>$checkout[date_back]</td></tr>";
+  echo "<tr><td>$checkout[hardware_id]</td><td>$checkout[users_id]</td><td style='background-color:orange;'>$checkout[date_out]</td><td style='background-color:green;'>$checkout[date_back]</td></tr>";
  }
  ?>
 </table>
