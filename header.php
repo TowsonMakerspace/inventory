b5494e72ab2b8dfb6e3bdd9f63c0041ba5f74efe
Patrick McGuire <insidenothing@gmail.com>
2017-09-12 14:31:58 -0400

    Update header.php

--- header.php
+++ header.php
@@ -19,17 +19,10 @@ if (isset($_POST['email']) && isset($_POST['password'])){
  setcookie("id", "1");
  header('Location: /inventory/');
 }
-?>
-<a href="https://www.towsonmaker.space"><img style="position: absolute; top:0px; right:0px;" height="100px" width="100px" src="http://www.towsonmaker.space/wp-content/uploads/2017/08/Towson.png"></a>
-<?PHP
 if (empty($_COOKIE['name'])){
  echo "<form method='post'>e:<input name='email'>p:<input name='password'><input type='submit'></form>";
  die();
-}else{
- echo "<h1>Welcome, $_COOKIE[name]</h1>";
 }
-
 // menu
 include_once('menu.php');
 ?>
-
