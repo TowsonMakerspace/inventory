4ef88eb3d22068152ae747837470e43d42b67f45
Patrick McGuire <insidenothing@gmail.com>
2017-09-11 20:56:24 -0400

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,7 +1,7 @@
 <?PHP if(empty($SECURITY)){ die('No direct file access'); } ?>
 <table border="1" width="100%" cellpadding="5" cellspacing="0"><tr>
- <td align="center"><a href='/inventory/'>HOME</a></td>
- <td align="center"><a href='terminal.php'>TERMINAL</a></td>
+ <td align="center"><a href='/inventory/'>Home</a></td>
+ <td align="center"><a href='terminal.php'>Terminal</a></td>
  <td align="center"><a href='inventory.php'>Inventory</a></td>
  <td align="center"><a href='members.php'>Members</a></td>
  <td align="center"><a href='logout.php'>Log Out<a/></td>
