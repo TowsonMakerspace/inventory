cfc45a3424c83b81f8e166a6635014ef24e293cb
Patrick McGuire <insidenothing@gmail.com>
2017-09-27 12:49:59 -0400

    Update label.php

--- label.php
+++ label.php
@@ -6,7 +6,7 @@ $r = mysqli_query($database, "select * from HARDWARE where ID = '$id' ");
 $hardware=mysqli_fetch_array($r,MYSQLI_ASSOC);
 ?>
 <center>
-  <div><img height='200' width='200' src='http://www.towsonmaker.space/wp-content/uploads/2017/08/Towson.png'></div>
+  <div><img height='300' width='300' src='http://www.towsonmaker.space/wp-content/uploads/2017/08/Towson.png'></div>
   <h1><?PHP echo $hardware['NAME']; ?></h1>
   <h3><?PHP echo $hardware['DESCRIPTION']; ?></h3>
   <div><img src='barcode.php?barcode=TMS-RENTAL-<?PHP echo $hardware['ID']; ?>'></div>
