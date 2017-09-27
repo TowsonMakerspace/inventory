d76cd044cd10ed6dd84ee629a28b6e57d2cfa211
Patrick McGuire <insidenothing@gmail.com>
2017-09-27 16:16:00 -0400

    Update label.php

--- label.php
+++ label.php
@@ -4,6 +4,10 @@ include('header_minimum.php');
 $id = $_GET['id'];
 $r = mysqli_query($database, "select * from HARDWARE where ID = '$id' ");
 $hardware=mysqli_fetch_array($r,MYSQLI_ASSOC);
+$sponsor = '';
+if ($hardware['SPONSOR'] != ''){
+  $sponsor = $hardware['SPONSOR']; 
+}
 ?>
 <center>
   <div class='title'>TOWSON</div>
@@ -11,12 +15,12 @@ $hardware=mysqli_fetch_array($r,MYSQLI_ASSOC);
   <div class='part'>Part: <?PHP echo $hardware['NAME']; ?></div>
   <div class='description'>Description: <?PHP echo $hardware['DESCRIPTION']; ?></div>
   <div><img src='barcode.php?barcode=TMS-RENTAL-<?PHP echo $hardware['ID']; ?>'></div>
-  <div class='sponsor'>Sponsor: <?PHP echo $hardware['SPONSOR']; ?></div>
+  <div class='sponsor'>Sponsor: <?PHP echo $sponsor; ?></div>
 </center>
 <style>
   body{ margin:0px; padding:0px; }  
   .title { font-size:30px; }
   .part { font-size:20px; }
   .description { font-size:15px; }
-  .sponsor { font-size:20px; border:5px black inset; font-variant:small_caps; padding:5px; margin:5px; }
+  .sponsor { font-size:20px; border:5px black ridge; font-variant:small_caps; padding:5px; margin:5px; }
 </style>
