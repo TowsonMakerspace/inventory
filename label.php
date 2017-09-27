33ba790572b5df6ed7de4dec33d6e61f71bf10d0
Patrick McGuire <insidenothing@gmail.com>
2017-09-27 16:10:57 -0400

    Update label.php

--- label.php
+++ label.php
@@ -11,12 +11,12 @@ $hardware=mysqli_fetch_array($r,MYSQLI_ASSOC);
   <div class='part'>Part: <?PHP echo $hardware['NAME']; ?></div>
   <div class='description'>Description: <?PHP echo $hardware['DESCRIPTION']; ?></div>
   <div><img src='barcode.php?barcode=TMS-RENTAL-<?PHP echo $hardware['ID']; ?>'></div>
-  <div class='sponsor'>Sponsor: <?PHP echo $hardware['DESCRIPTION']; ?></div>
+  <div class='sponsor'>Sponsor: <?PHP echo $hardware['SPONSOR']; ?></div>
 </center>
 <style>
   body{ margin:0px; padding:0px; }  
   .title { font-size:30px; }
   .part { font-size:20px; }
   .description { font-size:15px; }
-  .sponsor { font-size:20px; border:5px black inset; font-variant:small_caps; }
+  .sponsor { font-size:20px; border:5px black inset; font-variant:small_caps; padding:5px; margin:5px; }
 </style>
